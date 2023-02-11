db5255489a063a0ed95c42d16f154d25547d9b57
Yuriy-risk <111949781+Yuriy-risk@users.noreply.github.com>
2023-02-11 08:37:14 +0200

    Change in the project 7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,62 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+const galleryMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+    </div>`
+  )
+  .join("");
+
+galleryEl.innerHTML = galleryMarkup;
+galleryEl.addEventListener("click", onImageClick);
+
+let instance;
+
+function onImageClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  instance = basicLightbox.create(
+    `
+        <img src="${event.target.dataset.source}" width="800" height="600">
+    `,
+    {
+      onShow: () => {
+        escapeListener();
+      },
+      onClose: () => {
+        removeEscClose();
+      },
+    }
+  );
+
+  instance.show();
+}
+
+function escapeListener() {
+  window.addEventListener("keydown", closeWithEsc);
+}
+
+function closeWithEsc() {
+  /* console.log(event.code); */ // Event listener check
+  if (event.code === "Escape") {
+    instance.close();
+  }
+}
+
+function removeEscClose() {
+  window.removeEventListener("keydown", closeWithEsc);
+}
